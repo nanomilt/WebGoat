1b14b05edbdc192459a240fb05d59a50c9f4a736
MiltosNanos <nanomilt@ece.auth.gr>
2025-05-15 13:45:17 +0300

    claude-WebGoat Fixing Code-Violations

--- src/main/resources/lessons/idor/js/idor.js
+++ src/main/resources/lessons/idor/js/idor.js
@@ -1,18 +1,20 @@
 // need custom js for this?
 
-webgoat.customjs.idorViewProfile = function(data) {
-    webgoat.customjs.jquery('#idor-profile').html(
-        'name:' + data.name + '<br/>'+
-        'color:' + data.color + '<br/>'+
-        'size:' + data.size + '<br/>'
-    );
-}
+webgoat.customjs = webgoat.customjs || {};
 
-var onViewProfile = function () {
-    console.warn("on view profile activated");
-    webgoat.customjs.jquery.ajax({
-        method: "GET",
-        url: "IDOR/profile",
-        contentType: 'application/json; charset=UTF-8'
-     }).then(webgoat.customjs.idorViewProfile);
-}
+webgoat.customjs.idorViewProfile = function(data) {
+  webgoat.customjs.jquery('#idor-profile').html(
+    'name:' + data.name + '<br/>'+
+        'color:' + data.color + '<br/>'+
+        'size:' + data.size + '<br/>',
+  );
+};
+
+const onViewProfile = function () {
+  console.warn('on view profile activated');
+  webgoat.customjs.jquery.ajax({
+    method: 'GET',
+    url: 'IDOR/profile',
+    contentType: 'application/json; charset=UTF-8',
+  }).then(webgoat.customjs.idorViewProfile);
+};
