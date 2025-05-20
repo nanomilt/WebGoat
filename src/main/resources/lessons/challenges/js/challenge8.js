4ff95dcfdc861a08dc2e6cebb963785a1631170b
MiltosNanos <nanomilt@ece.auth.gr>
2025-05-20 15:26:42 +0300

    claude-WebGoat Fixing Code-Violations

--- src/main/resources/lessons/challenges/js/challenge8.js
+++ src/main/resources/lessons/challenges/js/challenge8.js
@@ -1,57 +1,55 @@
-$(document).ready(function () {
-    loadVotes();
-    average();
-})
+$(document).ready(() => {
+  loadVotes();
+  average();
+});
 
 function loadVotes() {
-    $.get("challenge/8/votes/", function (votes) {
-            let totalVotes = 0;
-            for (let i = 1; i <= 5; i++) {
-                totalVotes = totalVotes + votes[i];
-            }
-            console.log(totalVotes);
-            for (let i = 1; i <= 5; i++) {
-                const percent = votes[i] * 100 / totalVotes;
-                console.log(percent);
-                const progressBar = $('#progressBar' + i);
-                progressBar.width(Math.round(percent) * 2 + '%');
-                $("#nrOfVotes" + i).html(votes[i]);
-
-            }
-        }
-    );
+  $.get('challenge/8/votes/', (votes) => {
+    let totalVotes = 0;
+    for (let i = 1; i <= 5; i++) {
+      totalVotes += votes[i] || 0;
+    }
+    console.log(totalVotes);
+    for (let i = 1; i <= 5; i++) {
+      const percent = (votes[i] || 0) * 100 / totalVotes;
+      console.log(percent);
+      const progressBar = $(`#progressBar${ i}`);
+      progressBar.width(`${Math.round(percent) * 2 }%`);
+      $(`#nrOfVotes${ i}`).html(votes[i] || 0);
+    }
+  },
+  );
 }
 
 function average() {
-    $.get("challenge/8/votes/average", function (average) {
-            for (let i = 1; i <= 5; i++) {
-                const number = average["average"];
-                $("#star" + i).removeClass('btn-warning');
-                $("#star" + i).removeClass('btn-default');
-                $("#star" + i).removeClass('btn-grey');
+  $.get('challenge/8/votes/average', (average) => {
+    for (let i = 1; i <= 5; i++) {
+      const number = average.average || 0;
+      $(`#star${ i}`).removeClass('btn-warning');
+      $(`#star${ i}`).removeClass('btn-default');
+      $(`#star${ i}`).removeClass('btn-grey');
 
-                if (i <= number) {
-                    $("#star" + i).addClass('btn-warning');
-                } else {
-                    $("#star" + i).addClass('btn-grey');
-                }
-            }
-        }
-    );
+      if (i <= number) {
+        $(`#star${ i}`).addClass('btn-warning');
+      } else {
+        $(`#star${ i}`).addClass('btn-grey');
+      }
+    }
+  },
+  );
 }
 
-
 function doVote(stars) {
-    $("#voteResultMsg").hide();
-    $.get("challenge/8/vote/" + stars, function (result) {
-        if (result["error"]) {
-            $("#voteResultMsg").addClass('alert-danger alert-dismissable');
-        } else {
-            $("#voteResultMsg").addClass('alert-success alert-dismissable');
-        }
-        $("#voteResultMsg").html(result["message"]);
-        $("#voteResultMsg").show();
-    })
-    loadVotes();
-    average();
+  $('#voteResultMsg').hide();
+  $.get(`challenge/8/vote/${ stars}`, (result) => {
+    if (result.error) {
+      $('#voteResultMsg').addClass('alert-danger alert-dismissable');
+    } else {
+      $('#voteResultMsg').addClass('alert-success alert-dismissable');
+    }
+    $('#voteResultMsg').html(result.message);
+    $('#voteResultMsg').show();
+  });
+  loadVotes();
+  average();
 }
--- src/main/resources/lessons/jwt/js/jwt-refresh.js
+++ src/main/resources/lessons/jwt/js/jwt-refresh.js
@@ -1,42 +1,40 @@
-$(document).ready(function () {
-    login('Jerry');
-})
+$(document).ready(() => {
+  login('Jerry');
+});
 
-function login(user) {
-    $.ajax({
-        type: 'POST',
-        url: 'JWT/refresh/login',
-        contentType: "application/json",
-        data: JSON.stringify({user: user, password: "bm5nhSkxCXZkKRy4"})
-    }).success(
-        function (response) {
-            localStorage.setItem('access_token', response['access_token']);
-            localStorage.setItem('refresh_token', response['refresh_token']);
-        }
-    )
+function login(_) {
+  $.ajax({
+    type: 'POST',
+    url: 'JWT/refresh/login',
+    contentType: 'application/json',
+    data: JSON.stringify({user: _, password: 'bm5nhSkxCXZkKRy4'}),
+  }).success(
+    (response) => {
+      localStorage.setItem('access_token', response['access_token']);
+      localStorage.setItem('refresh_token', response['refresh_token']);
+    },
+  );
 }
 
-//Dev comment: Pass token as header as we had an issue with tokens ending up in the access_log
 webgoat.customjs.addBearerToken = function () {
-    var headers_to_set = {};
-    headers_to_set['Authorization'] = 'Bearer ' + localStorage.getItem('access_token');
-    return headers_to_set;
-}
+  const headers_to_set = {};
+  headers_to_set['Authorization'] = `Bearer ${ localStorage.getItem('access_token')}`;
+  return headers_to_set;
+};
 
-//Dev comment: Temporarily disabled from page we need to work out the refresh token flow but for now we can go live with the checkout page
 function newToken() {
-    var refreshToken = localStorage.getItem('refresh_token');
-    $.ajax({
-        headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('access_token')
-        },
-        type: 'POST',
-        url: 'JWT/refresh/newToken',
-        data: JSON.stringify({refreshToken: refreshToken})
-    }).success(
-        function (response) {
-            localStorage.setItem('access_token', response['access_token']);
-            localStorage.setItem('refresh_token', response['refresh_token']);
-        }
-    )
+  const refreshToken = localStorage.getItem('refresh_token');
+  $.ajax({
+    headers: {
+      'Authorization': `Bearer ${ localStorage.getItem('access_token')}`,
+    },
+    type: 'POST',
+    url: 'JWT/refresh/newToken',
+    data: JSON.stringify({refreshToken}),
+  }).success(
+    (response) => {
+      localStorage.setItem('access_token', response['access_token']);
+      localStorage.setItem('refresh_token', response['refresh_token']);
+    },
+  );
 }
